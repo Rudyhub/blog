050408db6e477994cae43a8500845a849b849ec0
Rudyhub <670752970@qq.com>
2018-08-22 00:14:23 +0800

    about

--- src/pages/about.vue
+++ src/pages/about.vue
@@ -1,37 +1,39 @@
 <template>
   <div class="about">
-    <!--<canvas class="about-cv" ref="canvas"></canvas>-->
-    <div class="about-screen">
-      <!--<p>我是谁并不重要，重要的是我能做什么。</p>-->
-      <!--<p>It's not important who I am, but what I can do.</p>-->
+    <div class="about-screen flex">
+      <p><img class="about-face" src="/static/face.png" alt="Rudy" draggable="false"></p>
+      <p class="fs20">我是谁并不重要，重要的是我能做什么。</p>
+      <p class="fs20">It's not important who I am, but what I can do.</p>
     </div>
     <div class="about-screen">
-      <!--<p>我是谁并不重要，重要的是我能做什么。</p>-->
-      <!--<p>It's not important who I am, but what I can do.</p>-->
+      <div class="fs20">技能：</div>
+      <p class="fs14">
+        先说关于“精通”、“熟练”、“掌握”和“了解”几个常用程度容易词，其实是文字游戏，没有参照物，任意水平都可以分出几个程度级别，自欺欺人。
+        所以，必须定一个参照。
+      </p>
+      <p class="fs14">
+        以Photoshop为参照，任意要求都可以，并且全程快捷键操作，并且同一个结果随意想出至少三种以上的方法来达到，以此视为精通，程度值为100%，则各技能程度值如下：
+      </p>
+      <div class="fs14">
+        <div class="about-item">
+          <span class="about-item-name">javascript:</span>
+          <span class="progress">
+            <i class="progress-value" style="width: 80%;">80%</i>
+          </span> <br>
+          <small class="about-small">描述：</small>
+        </div>
+      </div>
     </div>
     <div class="about-screen">
-      <!--<p>我是谁并不重要，重要的是我能做什么。</p>-->
-      <!--<p>It's not important who I am, but what I can do.</p>-->
     </div>
     <div class="about-screen">
-      <!--<p>我是谁并不重要，重要的是我能做什么。</p>-->
-      <!--<p>It's not important who I am, but what I can do.....</p>-->
     </div>
   </div>
 </template>
 
 <script>
-import tabs from '../components/tabs'
-import resume from './about/resume'
 export default {
   name: 'about',
-  components: {tabs},
-  data () {
-    return {
-      aboutMain: resume,
-      items: ['普通', '时间轴', '事件']
-    }
-  },
   mounted () {
     let _this, el, children, startY, angle, curangle, speeds, speed, timer
     _this = this
@@ -55,9 +57,11 @@ export default {
     }
     function end () {
       el.removeEventListener('mousemove', move)
-      speed = (speeds[0] - speeds[1]) / 10
       clearInterval(timer)
-      timer = setInterval(animteFrame, 16.6)
+      speed = (speeds[0] - speeds[1]) / 10
+      if (speeds[0] > 0 && speeds[1] > 0) {
+        timer = setInterval(animteFrame, 16.6)
+      }
     }
     function animteFrame () {
       window.cancelAnimationFrame(timer)
@@ -75,8 +79,7 @@ export default {
       children[2].style.transform = 'translateZ(-50vh) rotateX(' + (curangle + 180) + 'deg)'
       children[3].style.transform = 'translateZ(-50vh) rotateX(' + (curangle + 270) + 'deg)'
     }
-  },
-  methods: {}
+  }
 }
 </script>
 
@@ -87,29 +90,15 @@ export default {
     overflow: hidden;
     background: #07080d;
     perspective: 100vw;
-    color: #fff;
+    color: #ccd;
     user-select: none;
-    -webkit-user-drag: none;
   }
-  .about-cv{
-    position: absolute;
-    top: 0;
-    bottom: 0;
-    left: 0;
-    width: 100%;
-    height: calc(100vw * 800 / 1920);
+  .flex{
     display: flex;
-    margin: auto;
-    transform: rotateY(45deg);
-  }
-  .about-main{
-    background: rgba(215, 230, 255, 0.3);
-    height: 100%;
-    overflow: hidden;
-    perspective: 100vw;
-  }
-  .about-main-on{
-    animation: rotate-y-in .8s cubic-bezier(.57,.49,.16,1.23) forwards;
+    align-content: center;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
   }
   .about-screen{
     background: rgba(200, 240, 255, 0.2);
@@ -118,12 +107,10 @@ export default {
     height: 80vh;
     top: 0;
     left: 0;
-    display: flex;
-    align-content: center;
-    justify-content: center;
-    align-items: center;
     transform-origin: bottom;
     border: 3px solid #2e6881;
+    padding: .5em;
+    box-sizing: border-box;
   }
   .about-screen:nth-child(1){
     transform: translateZ(-50vh) rotateX(0);
@@ -136,5 +123,52 @@ export default {
   }
   .about-screen:nth-child(4){
     transform: translateZ(-50vh) rotateX(270deg)
+  }
+  .about-face{
+    width: 50%;
+    border-radius: 50%;
+    margin: 0 auto;
+    display: block;
+  }
+  .fs20{
+    font-size: 2vw;
+  }
+  .fs18{
+    font-size: 1.8vw;
+  }
+  .fs16{
+    font-size: 1.6vw;
+  }
+  .fs14{
+    font-size: 1.4vw;
+  }
+  .about-item{
+    border-bottom: 1px dotted #ccd;
+  }
+  .about-item-name{
+    line-height: 1;
+    vertical-align: middle;
+    display: inline-block;
+  }
+  .progress{
+    display: inline-block;
+    width: calc(100% - 40em);
+    background: rgba(255,255,255,.2);
+    position: relative;
+    border-radius: 1em;
+    line-height: 1;
+    overflow: hidden;
+    vertical-align: middle;
+    margin: 0 1em;
+  }
+  .progress-value{
+    display: inline-block;
+    background: rgba(111,174,9,.5);
+    border-radius: 1em;
+    text-align: center;
+  }
+  .about-small{
+    padding: .5em 0;
+    display: inline-block;
   }
 </style>
--- src/pages/about/resume.vue
+++ src/pages/about/resume.vue
@@ -1,20 +1,10 @@
 <template>
   <div class="resume">
     <div class="resume-face">
-<<<<<<< HEAD
       <!--<img src="../../../static/face.jpg" alt="">-->
       <p>我是谁并不重要，重要的是我能做什么。</p>
       <p>It's not important who I am, but what I can do.</p>
     </div>
-=======
-      <img src="../../../static/face.jpg" alt="">
-      <p class="resume-l">{{text[0]}}</p>
-    </div>
-    <drawrect class="draw-rect-1">{{text[1]}}</drawrect>
-    <drawrect class="draw-rect-2">{{text[2]}}</drawrect>
-    <drawrect class="draw-rect-3">{{text[3]}}</drawrect>
-    <drawrect class="draw-rect-4">{{text[4]}}</drawrect>
->>>>>>> 27d7067add365f76ed5886036ab24e8710207e3c
   </div>
 </template>
 
