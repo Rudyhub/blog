e879bdf80a7af6ae0742e1b0838e343d3ed74840
Rudyhub <670752970@qq.com>
2018-08-27 18:13:16 +0800

    todo table

--- src/pages/works.vue
+++ src/pages/works.vue
@@ -1,14 +1,12 @@
 <template>
   <div class="stage" :style="'perspective: '+stagePerspective+'vh'">
-    <div class="table" :style="{
-         transition: tableTransition,
-         transform: 'translateY('+tableTranslateY+'vh) rotateX(' + tableRotateX + 'deg) rotateZ(' + tableRotateZ + 'deg)',
-         }">
-      <div class="book" v-for="(book, index) of books" :key="index" @click="readBook(index)" :style="book.transform[book.status]">
+    <div class="table" :style="'transition:'+tableTransition+';transform:translateY('+tableTranslateY+'vh) rotateX(' + tableRotateX + 'deg) rotateZ(' + tableRotateZ + 'deg)'">
+      <div class="book" v-for="(book, index) of books" :key="index" @click="readBook(index)"
+           :style="'transform: scale3d('+bookScale+','+bookScale+','+bookScale+') rotateX(-90deg) rotateY(' + book.rotateY[book.status] + 'deg) translate3d('+book.translate3d[book.status]+')'">
         <bookcover :book="book" class="cover"/>
         <div class="spine spine-a">
-          <span class="spine-title" v-html="spineTextFilter(book.title)"></span>
-          <small class="text-dir-90">{{book.subtitle}}</small>
+          <div class="spine-title" v-html="spineTextFilter(book.title)"></div>
+          <div class="spine-subtitle" v-html="spineTextFilter(book.subtitle)"></div>
         </div>
         <div class="spine spine-b"></div>
         <div class="spine spine-c"></div>
@@ -16,7 +14,7 @@
         <bookcover :book="book" class="back-cover"/>
       </div>
     </div>
-    <div class="table-leg" :style="'transform: rotateX('+(tableRotateX+90)+'deg) translate3d(0, -'+(49.5+tableTranslateY)+'vh, -20vh);'"></div>
+    <div class="table-leg" :style="'transform:rotateX('+(tableRotateX+90)+'deg) translate3d(0, -'+(49.5+tableTranslateY)+'vh, -20vh)'"></div>
   </div>
 </template>
 
@@ -26,11 +24,11 @@ export default {
   name: 'works',
   components: {bookcover},
   data () {
-    let books, len, i
+    let books, len, i, rotateY, inits
     books = [
       {
         title: '规划设计类',
-        subtitle: '2008 在学校',
+        subtitle: '2008在学校',
         cover: '../../static/works/01/01.jpg'
       }, {
         title: '效果图类',
@@ -56,21 +54,26 @@ export default {
     ]
     len = books.length
     for (i = 0; i < len; i++) {
-      books[i].rotateY = Math.round(i * (360 / len))
+      rotateY = Math.round(i * (360 / len))
+      books[i].rotateY = Object.freeze([rotateY, rotateY - 90])
+      books[i].translate3d = Object.freeze(['-100vw, -50vh, 0', '0, -50vh, 35vh'])
       books[i].status = 0
-      books[i].transform = [
-        'transform: rotateX(-90deg) rotateY(' + books[i].rotateY + 'deg) translate3d(-21vh, -14.85vh, 0)',
-        'transform: rotateX(-90deg) rotateY(' + (books[i].rotateY - 90) + 'deg) translate3d(0, -14.85vh, 35vh)'
-      ]
     }
-    return {
+    inits = Object.freeze({
+      len,
       stagePerspective: 100,
-      books,
-      booksLen: len,
       tableRotateX: 60,
+      tableTranslateY: 0
+    })
+    return {
+      books,
+      inits,
+      stagePerspective: inits.stagePerspective,
+      tableRotateX: inits.tableRotateX,
       tableRotateZ: 130,
-      tableTranslateY: 0,
-      tableTransition: 'all 1s'
+      tableTranslateY: inits.tableTranslateY,
+      tableTransition: 'all 1s',
+      bookScale: 0.2
     }
   },
   mounted () {
@@ -84,11 +87,11 @@ export default {
       startX = e.clientX
       speedsX[0] = speedsX[1] = spX = 0
       curZ = z = _this.tableRotateZ
+      cancelAnimationFrame(timerX)
       _this.tableTransition = 'none'
       document.addEventListener('mousemove', move)
     }
     function move (e) {
-      _this.tableTransition = 'none'
       curZ = z + (startX - e.clientX) / 10
       speedsX.push(e.clientX)
       speedsX.shift()
@@ -96,7 +99,6 @@ export default {
     }
     document.addEventListener('mouseup', up)
     function up () {
-      _this.tableTransition = 'all 1s'
       document.removeEventListener('mousemove', move)
       spX = (speedsX[0] - speedsX[1]) / 10
       if (speedsX[0] > 0 && speedsX[1] > 0) {
@@ -107,7 +109,6 @@ export default {
     function easeOutX () {
       spX *= 0.96
       curZ += spX
-      _this.tableTransition = 'none'
       cancelAnimationFrame(timerX)
       timerX = requestAnimationFrame(easeOutX)
       if (Math.abs(spX) < 0.1) {
@@ -123,20 +124,20 @@ export default {
       return val.split('').join('<br>')
     },
     readBook (index) {
-      for (let i = 0; i < this.booksLen; i++) {
+      for (let i = 0; i < this.inits.booksLen; i++) {
         if (i !== index) {
           this.books[i].status = 0
         }
       }
       if (this.books[index].status) {
         this.books[index].status = 0
-        this.tableRotateX = 60
-        this.tableTranslateY = 0
-        this.stagePerspective = 100
+        this.tableRotateX = this.inits.tableRotateX
+        this.tableTranslateY = this.inits.tableTranslateY
+        this.stagePerspective = this.inits.stagePerspective
       } else {
         this.books[index].status = 1
         this.tableRotateX = 90
-        this.tableRotateZ = this.books[index].rotateY - 90 + Math.round(this.tableRotateZ / 360) * 360
+        this.tableRotateZ = this.books[index].rotateY[1] + Math.round(this.tableRotateZ / 360) * 360
         this.tableTranslateY = 14.85
         this.stagePerspective = 50
       }
@@ -146,38 +147,24 @@ export default {
 </script>
 
 <style>
-  h1{
-    font-size: 2vh;
-    line-height: 1;
-    padding: .5em;
-    border-bottom: 1px solid;
-    margin-top: 10vh;
-  }
   .stage{
     perspective: 100vh;
     height: 100vh;
     background: #07080d;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    align-content: center;
     overflow: hidden;
-    transition: all 1s;
   }
   .table{
     width: 80vh;
     height: 80vh;
-    position: absolute;
     border-radius: 50%;
     background: rgba(200, 240, 255, 0.2);
     border: 3px solid #2e6881;
-    transform: rotateX(60deg) rotateZ(130deg);
     transform-style: preserve-3d;
+    margin: 10vh auto;
     display: flex;
     align-items: center;
     justify-content: center;
     align-content: center;
-    z-index: 2;
   }
   .table:after{
     content: '';
@@ -191,19 +178,24 @@ export default {
     border-radius: 50%;
     overflow: hidden;
     opacity: 0.1;
+    position: absolute;
+    z-index: 0;
   }
   .table-leg{
     position: absolute;
     width: 3vh;
     height: 30vh;
     background: linear-gradient(90deg, #555 10%, #000000 30%, #bbd5ee 80%, #555);
-    top: 31vh;
+    top: -8vh;
+    left: 0;
+    right: 0;
+    bottom: 0;
+    margin: auto;
     transform-origin: top;
-    transform: rotateX(150deg) translate3d(0, -49.5vh, -20vh);
-    z-index: 1;
     transform-style: preserve-3d;
-    border-radius: 1vh 1vh 0 0;
+    border-radius: 1.5vh 1.5vh 0 0;
     transition: transform 1s;
+    z-index: -1;
   }
   .table-leg:before{
     content: '';
@@ -233,98 +225,88 @@ export default {
     user-select: none;
     cursor: pointer;
     transition: transform 1s;
+    width: 100vw;
+    height: 100vh;
+    z-index: 1;
   }
   .cover,
   .back-cover{
-    height: 29.7vh;
-    overflow: hidden;
+    width: 100vw;
+    height: 100vh;
     display: flex;
     align-items: center;
     justify-content: center;
     align-content: center;
     flex-direction: column;
     color: #333;
+    transform-style: preserve-3d;
   }
   .spine,
   .back-cover{
     position: absolute;
     top: 0;
   }
-  .cover,
-  .back-cover{
-    width: 21vh;
-  }
   .cover{
     background: linear-gradient(45deg, #888, #ccc);
     text-align: center;
   }
+  .back-cover{
+    transform: translateZ(-10vh) scaleX(-1);
+    background: linear-gradient(0, #c8c7d7, #c1b8ac);
+  }
   .spine{
     text-align: center;
-    font-size: 1.6vh;
+    font-size: 5vh;
     color: #333;
+    left: 0;
+    top: 0;
   }
   .spine-b,
   .spine-c,
   .spine-d{
-    width: calc(3vh - 2px);
+    width: calc(10vh - 2px);
     background: linear-gradient(90deg, #b9c3dd, #b9c3dd 50%, #c1b8ac 50%, #c1b8ac);
     background-size: 10% 100%;
   }
   .spine-a{
-    width: 3vh;
-    left: 0;
+    width: 10vh;
     transform-origin: left;
     transform: rotateY(-90deg) translateX(-100%);
-    box-shadow: inset 0 0 .5vh #fff;
-    height: 29.7vh;
+    box-shadow: inset 0 0 2vh #fff;
+    height: 100vh;
     background: linear-gradient(0, #b9c3dd, #c1b8ac);
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    align-content: center;
+    flex-direction: column;
   }
   .spine-b{
-    right: 0;
     transform-origin: right;
-    transform: rotateY(90deg)  translate3d(calc(3vh - 1px), 0.5vh, -0.5vh);
-    box-shadow: inset 0 0 .5vh #555;
-    height: 28.7vh;
+    transform: rotateY(90deg)  translate3d(calc(10vh - 1px), 2vh, calc(100vw - 12vh));
+    box-shadow: inset 0 0 2vh #555;
+    height: 96vh;
   }
   .spine-c{
-    right: 0;
     transform-origin: right top;
-    transform: rotateX(90deg) rotateZ(90deg) translate3d(-1px, calc(0.5vh - 1px), -0.5vh);
-    box-shadow: inset 0 0 .5vh #555;
-    height: 20.5vh;
+    transform: rotateX(90deg) rotateZ(90deg) translate3d(-1px, calc(9vh - 98vw), -2vh);
+    box-shadow: inset 0 0 2vh #555;
+    height: 98vw;
   }
   .spine-d{
-    left: 0;
-    bottom: 0;
     transform-origin: left bottom;
-    transform: rotateX(-90deg) rotateZ(90deg) translate3d(1px, -1px, 8.62vh);
-    box-shadow: inset 0 0 .5vh #555;
-    height: 20.5vh;
+    transform: rotateX(-90deg) rotateZ(90deg) translate3d(1px, -1px, -57vh);
+    box-shadow: inset 0 0 2vh #555;
+    height: 98vw;
   }
   .spine-title{
-    padding-top: .5em;
-    display: inline-block;
+    margin: 0 auto;
   }
-  .back-cover{
-    transform: translateZ(-3vh) scaleX(-1);
-    background: linear-gradient(0, #c8c7d7, #c1b8ac);
-  }
-  .text-dir-90{
-    transform: rotateZ(90deg);
-    transform-origin: left bottom;
-    display: inline-block;
-    line-height: 3vh;
-    white-space: nowrap;
-    text-transform: uppercase;
-  }
-  .cover-text{
-    text-transform: uppercase;
-    font-size: .8em;
-  }
-  .cover-img{
-    width: 80%;
-    top: 3vh;
-    position: absolute;
-    transform: translateZ(1px);
+  .spine-subtitle{
+    background: #333;
+    color: #fff;
+    padding: 1vh;
+    margin-top: 3vh;
+    font-size: 0.8em;
   }
 </style>
--- src/pages/works/bookcover.vue
+++ src/pages/works/bookcover.vue
@@ -2,8 +2,8 @@
   <div>
     <canvasplayer v-if="book.video" :src="book.video" :autoplay="true" :loop="true" class="cover-img"></canvasplayer>
     <img class="cover-img" :src="book.cover" draggable="false">
-    <h1>{{book.title}}</h1>
-    <p class="cover-text">{{book.subtitle}}</p>
+    <h1 class="cover-title">{{book.title}}</h1>
+    <h2 class="cover-subtitle">{{book.subtitle}}</h2>
   </div>
 </template>
 
@@ -15,3 +15,20 @@ export default {
   props: ['book']
 }
 </script>
+<style>
+  .cover-title{
+    font-size: 6vh;
+    line-height: 1;
+    padding: .5em;
+    border-bottom: 1px solid;
+  }
+  .cover-subtitle{
+    text-transform: uppercase;
+    font-size: 5vh;
+    margin: 0;
+  }
+  .cover-img{
+    max-height: 40vh;
+    max-width: 80%;
+  }
+</style>
